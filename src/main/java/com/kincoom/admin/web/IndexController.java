31511d7e19599b790497a7072382830111906983
kincoom <kincoom@gmail.com>
2020-02-20 20:01:21 +0900

    oauth2.0

--- src/main/java/com/kincoom/admin/web/IndexController.java
+++ src/main/java/com/kincoom/admin/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.kincoom.admin.web;
 
+import com.kincoom.admin.config.auth.LoginUser;
+import com.kincoom.admin.config.auth.dto.SessionUser;
 import com.kincoom.admin.service.posts.PostsService;
 import com.kincoom.admin.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +10,32 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){  //어떤 컨트롤러든지 @LoginUser만 사용하면 세션정보를 가져올 수 있음.
         model.addAttribute("posts", postsService.findAllDesc());
+
+        /**
+         * CustomOAuth2UserService에서 로그인 성공 시 세션에 SessionUser를 저장하도록 구성
+         * 즉, 로그인 성공시 httpSession.getAttribute("user")에서 값을 가져올 수 있음
+         * */
+        //SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        /**
+         * 세션에 저장된 값이 있을 때만 model에 userName으로 등록
+         * 세션에 저장된 값이 없으면 model엔 아무런 값이 없는 상태이니 로그인 버튼이 보이게 됨
+         * */
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
@@ -31,5 +50,7 @@ public class IndexController {
         model.addAttribute("post", dto);
 
         return "posts-update";
+
+
     }
 }
